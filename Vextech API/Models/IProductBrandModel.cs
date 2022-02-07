5cc4f10e064c052dec659a3e93e99f3ff98de7e1
noahkuhne12 <50324963+noahkuhne12@users.noreply.github.com>
2022-02-07 09:08:44 +0100

    create brandModels

---
+++ Vextech API/Models/IProductBrandModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Vextech_API.Models
+{
+    public interface IProductBrandModel
+    {
+        string Brand { get; set; }
+        int ID { get; }
+    }
+}
--- Vextech API/Models/ProductBrandModel.cs
+++ Vextech API/Models/ProductBrandModel.cs
@@ -1,6 +1,9 @@
 ﻿namespace Vextech_API.Models
 {
-    public class ProductBrandModel
+    public class ProductBrandModel : IProductBrandModel
     {
+        public string Brand { get; set; }
+
+        public int ID { get; }
     }
 }
