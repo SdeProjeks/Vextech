c9cf824bb728ff4347cca2d9606d61c0bdfa5143
Hopewow <43467655+Hopewow@users.noreply.github.com>
2022-02-08 11:17:00 +0100

    Made changes to getproduct endpoints so there are admin and user specific endpoints differences being admins can see inactive products. Also created an activate and inactivate product endpoints.

--- Vextech API/Controllers/ProductController.cs
+++ Vextech API/Controllers/ProductController.cs
@@ -15,12 +15,12 @@ namespace Vextech_API.Controllers
         public List<ProductCategoryNameModel> categories { get; set; }
 
         [HttpGet]
-        public ActionResult<List<ProductModel>> GetAllProducts()
+        public ActionResult<List<ProductModel>> GetProducts()
         {
             try
             {
                 Products = new();
-                string sql = "SELECT products.ID, products.Name, products.Active, products.Price, products.Release_date, products.BrandID, product_brand.Brand FROM products INNER JOIN product_brand ON products.BrandID = product_brand.ID;";
+                string sql = "SELECT products.ID, products.Name, products.Active, products.Price, products.Release_date, products.BrandID, product_brand.Brand FROM products INNER JOIN product_brand ON products.BrandID = product_brand.ID WHERE products.Active = 1;";
                 var DatabaseResults = SqlDataAccess.LoadData<VProductModel>(sql);
 
                 foreach (var item in DatabaseResults)
@@ -71,7 +71,7 @@ namespace Vextech_API.Controllers
         {
             try
             {
-                string sql = $"SELECT products.ID, products.Name, products.Description, products.Active, products.Price, products.Release_date, products.BrandID, product_brand.Brand FROM products INNER JOIN product_brand ON products.BrandID = product_brand.ID WHERE products.ID = {id};";
+                string sql = $"SELECT products.ID, products.Name, products.Description, products.Active, products.Price, products.Release_date, products.BrandID, product_brand.Brand FROM products INNER JOIN product_brand ON products.BrandID = product_brand.ID WHERE products.ID = {id} AND products.Active = 1;";
                 var DatabaseResults = SqlDataAccess.LoadData<VProductModel>(sql);
 
                 if (DatabaseResults.Count > 0)
@@ -113,7 +113,114 @@ namespace Vextech_API.Controllers
                     return products;
                 }
                 
-                return this.StatusCode(StatusCodes.Status404NotFound, "Product you searched does not exist");
+                return this.StatusCode(StatusCodes.Status404NotFound, "Product you searched for does not exist");
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult<List<ProductModel>> AdminGetAllProducts()
+        {
+            try
+            {
+                Products = new();
+                string sql = "SELECT products.ID, products.Name, products.Active, products.Price, products.Release_date, products.BrandID, product_brand.Brand FROM products INNER JOIN product_brand ON products.BrandID = product_brand.ID;";
+                var DatabaseResults = SqlDataAccess.LoadData<VProductModel>(sql);
+
+                foreach (var item in DatabaseResults)
+                {
+                    sql = $"SELECT product_categories.ProductID, product_categories.CategoryID, product_category_names.Subcategory, product_category_names.Category FROM product_categories INNER JOIN product_category_names ON product_categories.CategoryID = product_category_names.ID WHERE product_categories.ProductID = {item.ID};";
+                    var CategoriesResult = SqlDataAccess.LoadData<VProductCategoriesModel>(sql);
+                    categories = new();
+
+                    foreach (var Databasecategory in CategoriesResult)
+                    {
+                        ProductCategoryNameModel category = new()
+                        {
+                            ID = Databasecategory.CategoryID,
+                            Subcategory = Databasecategory.Subcategory,
+                            Category = Databasecategory.Category
+                        };
+                        categories.Add(category);
+                    }
+
+                    ProductModel products = new()
+                    {
+                        ID = item.ID,
+                        Name = item.Name,
+                        Price = item.Price,
+                        Active = item.Active,
+                        Release_date = item.Release_date,
+                        Brand = new()
+                        {
+                            ID = item.BrandID,
+                            Brand = item.Brand
+                        },
+                        Categories = categories
+                    };
+
+                    Products.Add(products);
+                }
+
+                return Products;
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<ProductModel> AdminGetProduct(int id)
+        {
+            try
+            {
+                string sql = $"SELECT products.ID, products.Name, products.Description, products.Active, products.Price, products.Release_date, products.BrandID, product_brand.Brand FROM products INNER JOIN product_brand ON products.BrandID = product_brand.ID WHERE products.ID = {id};";
+                var DatabaseResults = SqlDataAccess.LoadData<VProductModel>(sql);
+
+                if (DatabaseResults.Count > 0)
+                {
+                    // Gets all the products categories:
+                    sql = $"SELECT product_categories.ProductID, product_categories.CategoryID, product_category_names.Subcategory, product_category_names.Category FROM product_categories INNER JOIN product_category_names ON product_categories.CategoryID = product_category_names.ID WHERE product_categories.ProductID = {id};";
+                    var CategoriesResult = SqlDataAccess.LoadData<VProductCategoriesModel>(sql);
+
+                    // Creates new list to handle all the products categories:
+                    categories = new();
+                    foreach (var Databasecategory in CategoriesResult)
+                    {
+                        ProductCategoryNameModel category = new()
+                        {
+                            ID = Databasecategory.CategoryID,
+                            Subcategory = Databasecategory.Subcategory,
+                            Category = Databasecategory.Category
+                        };
+                        categories.Add(category);
+                    }
+
+                    // Creates a single product from a model
+                    ProductModel products = new()
+                    {
+                        ID = DatabaseResults[0].ID,
+                        Name = DatabaseResults[0].Name,
+                        Descrption = DatabaseResults[0].Description,
+                        Price = DatabaseResults[0].Price,
+                        Active = DatabaseResults[0].Active,
+                        Release_date = DatabaseResults[0].Release_date,
+                        Brand = new()
+                        {
+                            ID = DatabaseResults[0].BrandID,
+                            Brand = DatabaseResults[0].Brand
+                        },
+                        Categories = categories
+                    };
+
+                    return products;
+                }
+
+                return this.StatusCode(StatusCodes.Status404NotFound, "Product you searched for does not exist");
             }
             catch (Exception)
             {
@@ -144,11 +251,132 @@ namespace Vextech_API.Controllers
                     return this.StatusCode(StatusCodes.Status500InternalServerError, "Some database issue has been encountered your product was not created.");
                 }
 
+
+                foreach (var category in categories)
+                {
+                    VProductCategoriesModel categoryModel = new()
+                    {
+                        CategoryID = category
+                    };
+                    sql = @"INSERT INTO product_categories (product_categories.ProductID, product_categories.CategoryID) VALUES ((SELECT MAX(ID) FROM products), @CategoryID);";
+                    results = SqlDataAccess.SaveData<VProductCategoriesModel>(sql, categoryModel);
+                }
+
                 return Ok("Product was created successfully");
             }
             catch (Exception)
             {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+            }
+        }
+
+        [HttpPut]
+        public ActionResult UpdateProduct(int id, string name, string description, int brandID, decimal price, DateTime? release_date, int active, List<int> categories)
+        {
+            try
+            {
+                VProductModel data = new()
+                {
+                    ID = id,
+                    Name = name,
+                    Description = description,
+                    BrandID = brandID,
+                    Price = price,
+                    Active = active
+                };
+
+                // Update product
+                string sql = @"UPDATE products SET Name=@Name, Description=@Description, BrandID=@BrandID, Price=@Price, Release_date=@Release_date, Active=@Active WHERE ID = @ID;";
+                var results = SqlDataAccess.SaveData<VProductModel>(sql,data);
+
+                if (results == 0)
+                {
+                    return this.StatusCode(StatusCodes.Status500InternalServerError, "Some database issue has been encountered your product was not updated.");
+                }
+
+                // Delete all connected categories to then reapply them later
+                sql = $"DELETE FROM product_categories WHERE ProductID = {id}";
+                results = SqlDataAccess.DeleteData(sql);
+
+                if (results == 0)
+                {
+                    return this.StatusCode(StatusCodes.Status500InternalServerError, "Some database issue has been encountered your product did either not have any categories or database failed with deletion.");
+                }
+
+                // Create all the relation to the products categories.
+                foreach (var category in categories)
+                {
+                    VProductCategoriesModel categoryModel = new()
+                    {
+                        ProductID = id,
+                        CategoryID = category
+                    };
+                    sql = @"INSERT INTO product_categories (product_categories.ProductID, product_categories.CategoryID) VALUES (@ProductID, @CategoryID);";
+                    results = SqlDataAccess.SaveData<VProductCategoriesModel>(sql, categoryModel);
+                }
+
+                return Ok("Product was updated successfully");
+            }
+            catch (Exception)
+            {
                 throw;
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+            }
+        }
+
+
+        [HttpPut]
+        public ActionResult InactivateProduct(int id)
+        {
+            try
+            {
+                VProductModel data = new()
+                {
+                    ID = id,
+                    Active = 0
+                };
+
+                // Update product
+                string sql = @"UPDATE products SET Active=@Active WHERE ID = @ID;";
+                var results = SqlDataAccess.SaveData<VProductModel>(sql, data);
+
+                if (results == 0)
+                {
+                    return this.StatusCode(StatusCodes.Status500InternalServerError, "Some database issue has been encountered your product has not been made inactive.");
+                }
+
+                return Ok("Product was updated successfully made inactive");
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+            }
+        }
+
+        [HttpPut]
+        public ActionResult ActivateProduct(int id)
+        {
+            try
+            {
+                VProductModel data = new()
+                {
+                    ID = id,
+                    Active = 1
+                };
+
+                // Update product
+                string sql = @"UPDATE products SET Active=@Active WHERE ID = @ID;";
+                var results = SqlDataAccess.SaveData<VProductModel>(sql, data);
+
+                if (results == 0)
+                {
+                    return this.StatusCode(StatusCodes.Status500InternalServerError, "Some database issue has been encountered your product has not been made inactive.");
+                }
+
+                return Ok("Product was updated successfully made inactive");
+            }
+            catch (Exception)
+            {
                 return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
             }
         }
