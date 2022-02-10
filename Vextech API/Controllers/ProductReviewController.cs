8a63c35aa2388690c82cdba6789c2457f2a18d2a
willjon2000 <william.s.anker@gmail.com>
2022-02-10 13:16:39 +0100

    Made crud for product review

--- Vextech API/Controllers/ProductReviewController.cs
+++ Vextech API/Controllers/ProductReviewController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Vextech_API.Models;
+using Vextech_API.Models.ViewModels;
+using Vextech_API.DataAccess;
 
 namespace Vextech_API.Controllers
 {
@@ -7,5 +10,216 @@ namespace Vextech_API.Controllers
     [ApiController]
     public class ProductReviewController : ControllerBase
     {
+        public List<ProductReviewModel> productReviews {get; set;}
+
+        [HttpGet]
+        public ActionResult<List<ProductReviewModel>> GetAllReviews()
+        {
+            try
+            {
+                productReviews = new();
+                string sql;
+                sql = "SELECT products.Name, products.Price, users.Firstname, users.Lastname, product_reviews.ID, product_reviews.`Comment`, product_reviews.Rating, product_reviews.Date FROM product_reviews INNER JOIN users ON product_reviews.UserID = users.ID INNER JOIN products ON product_reviews.ProductID = products.ID;";
+
+                var result = SqlDataAccess.LoadData<VProductReviewModel>(sql);
+                foreach (var Review in result)
+                {
+                    ProductReviewModel remaping = new()
+                    {
+                        ID = Review.ID,
+                        Product = new()
+                        {
+                            Name = Review.Name,
+                            Price = Review.Price
+                        },
+                        User = new()
+                        {
+                            Firstname = Review.Firstname,
+                            Lastname = Review.Lastname
+                        },
+                        Comment = Review.comment,
+                        Rating = Review.Rating
+                    };
+                    productReviews.Add(remaping);
+                }
+                if (productReviews.Count == 0)
+                {
+                    return this.StatusCode(StatusCodes.Status204NoContent, "There are no product reviews");
+                }
+                return productReviews;
+                
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+            }
+        }
+        [HttpGet("{id:int}")]
+        public ActionResult<List<ProductReviewModel>> GetOneReview(int id)
+        {
+            try
+            {
+                productReviews = new();
+                string sql;
+                sql = $"SELECT product_reviews.ID, products.Name, products.Price, products.Description, product_brand.Brand, users.Firstname, users.Lastname, product_reviews.`Comment`, product_reviews.Rating, product_reviews.Date FROM product_reviews INNER JOIN users ON product_reviews.UserID = users.ID INNER JOIN products ON product_reviews.ProductID = products.ID INNER JOIN product_brand ON product_brand.ID = products.BrandID WHERE product_reviews.ID = {id};";
+
+                var result = SqlDataAccess.LoadData<VProductReviewModel>(sql);
+                foreach (var Review in result)
+                {
+                    ProductReviewModel reviewing = new()
+                    {
+                        ID = Review.ID,
+                        Product = new()
+                        {
+                            Name = Review.Name,
+                            Price = Review.Price,
+                            Descrption = Review.Decription,
+                            Brand = new()
+                            {
+                                ID = Review.BrandID,
+                                Brand = Review.Brand
+                            },
+                        },
+                        User = new()
+                        {
+                            Firstname = Review.Firstname,
+                            Lastname = Review.Lastname,
+                        },
+                        Comment = Review.comment,
+                        Rating = Review.Rating
+                    };
+                    productReviews.Add(reviewing);
+                }
+                if (productReviews.Count == 0)
+                {
+                    return this.StatusCode(StatusCodes.Status404NotFound, "Could not find the review");
+                }
+                return productReviews;
+                
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+            }
+        }
+        [HttpGet("{productID:int}")]
+        public ActionResult<List<ProductReviewModel>> GetAllReviewsToOneProduct (int productID)
+        {
+            try
+            {
+                productReviews = new();
+                string sql;
+                sql = $"SELECT product_reviews.ID, product_reviews.`Comment`, product_reviews.Rating, product_reviews.Date, product_reviews.ProductID, products.Name, products.Price, products.Description, products.BrandID, product_brand.Brand, product_reviews.UserID, users.Firstname, users.Lastname FROM product_reviews INNER JOIN users ON product_reviews.UserID = users.ID INNER JOIN products ON product_reviews.ProductID = products.ID INNER JOIN product_brand ON products.BrandID = product_brand.ID WHERE products.ID = {productID};";
+
+                var result = SqlDataAccess.LoadData<VProductReviewModel>(sql);
+                foreach (var Review in result)
+                {
+                    ProductReviewModel reviewing = new()
+                    {
+                        ID = Review.ID,
+                        Product = new()
+                        { 
+                            ID = Review.ProductID,
+                            Name = Review.Name,
+                            Price = Review.Price,
+                            Descrption = Review.Decription,
+                            Brand = new()
+                            {
+                                ID = Review.BrandID,
+                                Brand = Review.Brand
+                            },
+                        },
+                        User = new()
+                        {
+                            ID = Review.UserID,
+                            Firstname = Review.Firstname,
+                            Lastname = Review.Lastname,
+                        },
+                        Comment = Review.comment,
+                        Rating = Review.Rating
+                    };
+                    productReviews.Add(reviewing);
+                }
+                if (productReviews.Count == 0)
+                {
+                    return this.StatusCode(StatusCodes.Status204NoContent, "There is no product reviews to the product");
+                }
+                return productReviews;
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult CreateProductReview(ulong UserID, int ProductID, string Comment, int Rating)
+        {
+            try
+            {
+                VProductReviewModel data = new()
+                {
+                    ProductID = ProductID,
+                    UserID = UserID,
+                    comment = Comment,
+                    Rating = Rating,
+                };
+
+                string sql;
+                sql = @"INSERT INTO product_reviews (ProductID,UserID, Comment, Rating) VALUES (@ProductID, @UserID, @comment, @Rating);";
+
+                var result = SqlDataAccess.SaveData<VProductReviewModel>(sql, data);
+
+                return Ok("Review has been added");
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status400BadRequest, "Create failed because of invalid input");
+            }
+        }
+
+        [HttpPut]
+        public ActionResult UpdateProductReview(ulong ID, string Comment, int Rating)
+        {
+            try
+            {
+                string sql;
+                sql = $"UDPATE product_reviews SET Comment='{Comment}', Rating = {Rating} WHERE ID = {ID}";
+                
+                var result = SqlDataAccess.UpdateData(sql);
+
+                return Ok("Updated the review succesfully");
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status404NotFound, "Update failed because review was not found in the database");
+            }
+        }
+
+        public ActionResult DeleteOneReview(int id, int userID)
+        {
+            try
+            {
+                string sql;
+
+                // When checking for permissions
+                if (false)
+                {
+
+                }
+                else
+                {
+                    sql = $"DELETE FROM product_reviews WHERE ID = {id} AND UserID = {userID}";
+                    var result = SqlDataAccess.DeleteData(sql);
+
+                    return Ok("Deleted your review succesfully");
+                }
+
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status404NotFound, "could not delete the review because we could not find it review");
+            }
+        }
     }
 }
--- Vextech API/Models/ProductReviewModel.cs
+++ Vextech API/Models/ProductReviewModel.cs
@@ -1,6 +1,11 @@
 ﻿namespace Vextech_API.Models
 {
-    public class VProductReviewModel
+    public class ProductReviewModel
     {
+        public ulong ID { get; set; }
+        public ProductModel Product { get; set; }
+        public UserModel User { get; set; }
+        public string Comment { get; set; }
+        public int Rating { get; set; }
     }
 }
