badec636a72465abc76b7eaf4ed3ad61a485b03d
Hopewow <43467655+Hopewow@users.noreply.github.com>
2022-02-10 08:56:05 +0100

    Created getall and sepific contact, create contact and delete contact (used when done with a contact).

--- Vextech API/Controllers/ContactController.cs
+++ Vextech API/Controllers/ContactController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Vextech_API.Models;
+using Vextech_API.DataAccess;
 
 namespace Vextech_API.Controllers
 {
@@ -7,5 +9,90 @@ namespace Vextech_API.Controllers
     [ApiController]
     public class ContactController : ControllerBase
     {
+        [HttpPost]
+        public ActionResult CreateContact(string name, string email, string message)
+        {
+            try
+            {
+
+                ContactModel data = new()
+                {
+                    Name = name,
+                    Email = email,
+                    Message = message
+                };
+
+                string sql = @"INSERT INTO contacts (Name, Email, Message) VALUES (@Name, @Email, @Message);";
+                var result = SqlDataAccess.SaveData<ContactModel>(sql, data);
+
+                if (result == 0)
+                {
+                    return this.StatusCode(StatusCodes.Status500InternalServerError,"The database encountered an error and your message was not delivered.");
+                }
+
+                return Ok("Your message has been recieved.");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpGet]
+        public ActionResult<List<ContactModel>> GetAllContacts()
+        {
+            try
+            {
+                var sql = "SELECT * FROM contacts";
+                var result = SqlDataAccess.LoadData<ContactModel>(sql);
+
+                return result;
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<List<ContactModel>> GetContact(int id)
+        {
+            try
+            {
+                var sql = $"SELECT * FROM contacts WHERE ID={id}";
+                var result = SqlDataAccess.LoadData<ContactModel>(sql);
+
+                return result;
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+
+        [HttpDelete]
+        public ActionResult DeleteContact(int contactID)
+        {
+            try
+            {
+
+                string sql = $"DELETE FROM contacts WHERE ID={contactID};";
+                var result = SqlDataAccess.DeleteData(sql);
+
+                if (result == 0)
+                {
+                    return this.StatusCode(StatusCodes.Status500InternalServerError, "The database encountered an issue your contact was not deleted cause it could not be found.");
+                }
+
+                return Ok("Contact was successfully deleted.");
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+
+
     }
 }
--- Vextech API/Models/ContactModel.cs
+++ Vextech API/Models/ContactModel.cs
@@ -2,5 +2,9 @@
 {
     public class ContactModel
     {
+        public ulong ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
     }
 }
