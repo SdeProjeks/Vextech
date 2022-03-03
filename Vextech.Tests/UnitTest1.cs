bb0672223605acb2706cb11c9cd692086df57faa
Hopewow <43467655+Hopewow@users.noreply.github.com>
2022-03-03 08:56:55 +0100

    updated some models and made an example unit test.

--- Vextech.Tests/UnitTest1.cs
+++ Vextech.Tests/UnitTest1.cs
@@ -7,7 +7,16 @@ namespace Vextech.Tests
         [Fact]
         public void Test1()
         {
+            // Arrange
+            var expected = "Tim Sommerstedt";
+            var firstname = "Tim";
+            var secondname = "Sommerstedt";
 
+            // Act
+            var actual = firstname + " " + secondname;
+
+            // Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
